b504ab075d68b1cad840742d5e2dccd620c6ba18
trevorsaudi <isatrevor052@gmail.com>
2021-01-01 20:46:48 +0300

    a working bash script

--- automate.sh
+++ automate.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/bash
 
-mkcd(){
+repo_name=$1
 
-	mkdir -p $1 && cd $1 
+makecd(){
+  mkdir -p $repo_name && cd $repo_name
+  echo "$repo_name" > README.md
+  git init
+  git add .
+  git commit -m "first commit"
+  git branch -M main
+  curl -i -H "Authorization: token $access_token" -d '{"name": "'"$repo_name"'","auto_init": false,"private": true}'  https://api.github.com/user/repos
+  git remote add origin https://github.com/trevorsaudi/${repo_name}.git
+  git push -u origin main
+  code .
+  exec bash
 
 }
+makecd $repo_name
 
-
-gitinitalize(){
-	git init
-	echo "# $1" >> README.md
-	git add .
-	git commit -m "intial commit"
-	git branch -M main
-	git remote add origin https://github.com/trevorsaudi/$1.git
-	git push -u origin main
-	exec bash
-}
-mkcd $1
-gitinitalize $1
-exec()
--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 import os
-import requests
+# import requests
+import sys
+import subprocess
 
 access_token = os.getenv('access_token')
 
@@ -7,7 +9,11 @@ headers = {
     'Authorization': f"token {access_token}",
 }
 
-data =  '{ "name": "test", "auto_init": true, "private": true, "gitignore_template": "nanoc" }'
 
+repo_name = sys.argv[1]
+cwd = os.getcwd()
+os.mkdir(repo_name)
+os.chdir(cwd + '/' + repo_name)
+
+data =  '{ "name": "%s", "auto_init": true, "private": true, "gitignore_template": "nanoc" }' %(repo_name)
 response = requests.post('https://api.github.com/user/repos', headers=headers, data=data)
-print(response.content)
